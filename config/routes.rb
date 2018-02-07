c2f4ad1b7822ab3cf13c7230634f1ca5643aa16e
Oleg Kelin <rtpr2@yandex.ru>
2018-02-08 00:27:36 +0300

    routes index root

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Ms::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root to: 'store#index', as: 'store'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
